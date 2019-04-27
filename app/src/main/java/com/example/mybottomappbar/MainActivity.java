76bd704adf10ba2f56cb00a541daacb8e3f63271
Metin Yıldız <metinyildiz2006@gmail.com>
2019-04-27 14:07:25 +0300

    Adding RecyclerView - CardView

--- app/src/main/java/com/example/mybottomappbar/MainActivity.java
+++ app/src/main/java/com/example/mybottomappbar/MainActivity.java
@@ -5,15 +5,21 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
-    private CoordinatorLayout coordinator_layout;
-    private BottomAppBar bar;
-    private FloatingActionButton fab;
+    public static CoordinatorLayout coordinator_layout;
+    public static BottomAppBar bar;
+    public static FloatingActionButton fab;
+    private RecyclerView rv;
+    private MyAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +27,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tanimla();
+        setUpRecyclerView();
 
+
+    }
+
+    private void tanimla() {
+        coordinator_layout = findViewById(R.id.coordinator_layout);
+        bar = findViewById(R.id.bottom_app_bar);
+        fab = findViewById(R.id.fab);
+        rv = findViewById(R.id.rv);
+
+        setSupportActionBar(bar);
+
+        clickProccess();
+    }
+
+    private void clickProccess() {
         bar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,23 +59,37 @@ public class MainActivity extends AppCompatActivity {
                 showSnackbar("Clicked fab");
             }
         });
-
-
     }
 
-    private void tanimla() {
-        coordinator_layout = findViewById(R.id.coordinator_layout);
-        bar = findViewById(R.id.bottom_app_bar);
-        fab = findViewById(R.id.fab);
+    private void setUpRecyclerView() {
+        rv.setHasFixedSize(true);
+        rv.setLayoutManager(new LinearLayoutManager(MainActivity.this));
 
-        setSupportActionBar(bar);
+        ArrayList<String> countries = new ArrayList<>();
 
+        countries.add("TURKEY");
+        countries.add("USA");
+        countries.add("CANADA");
+        countries.add("JAPAN");
+        countries.add("ITALY");
+        countries.add("FRANCE");
+        countries.add("GERMANY");
+        countries.add("FINLAND");
+        countries.add("RUSSIA");
+        countries.add("GREECE");
+        countries.add("ARGENTINA");
+        countries.add("BRAZIL");
+        countries.add("INDIA");
+        countries.add("CHINA");
 
+        adapter = new MyAdapter(countries, MainActivity.this, coordinator_layout);
+        rv.setAdapter(adapter);
     }
 
-    private void showSnackbar(CharSequence text) {
+    public static void showSnackbar(CharSequence text) {
         Snackbar.make(coordinator_layout, text, Snackbar.LENGTH_SHORT)
                 .setAnchorView(fab.getVisibility() == View.VISIBLE ? fab : bar)
-                .show();
+                .show(); // floating action button'un üstünde gösteriyor.
     }
+
 }
